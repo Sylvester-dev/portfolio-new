7f2fbbec067f55653227c1340fe25d06c11ac7a8
Vineet Kumar <vineetkumar@Vineets-MacBook-Air-2.local>
2025-04-13 16:14:15 +0530

    fix

--- components/ui/BentoGrid.tsx
+++ components/ui/BentoGrid.tsx
@@ -52,8 +52,8 @@ export const BentoGridItem = ({
   titleClassName?: string;
   spareImg?: string;
 }) => {
-  const leftLists = ["ReactJS", "Express", "Typescript"];
-  const rightLists = ["VueJS", "NuxtJS", "GraphQL"];
+  const leftLists = ["Low code", "Bubble", "Lovable"];
+  const rightLists = ["Make", "NextJS", "MERN"];
 
   const [copied, setCopied] = useState(false);
 
@@ -67,7 +67,7 @@ export const BentoGridItem = ({
   };
 
   const handleCopy = () => {
-    const text = "hsu@jsmastery.pro";
+    const text = "madebyvineet@gmail.com";
     navigator.clipboard.writeText(text);
     setCopied(true);
   };
